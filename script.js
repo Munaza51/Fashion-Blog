12ff5d6dbe5bb79c6d4f671b15e4af419522dc46
munaza hazrati <63694714+Munaza51@users.noreply.github.com>
2024-06-29 18:24:17 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const featuredPostImage = document.querySelector('.featured-post img');
+
+featuredPostImage.addEventListener('click', () => {
+  window.location.href = "#"; // Replace with actual featured post link
+});
